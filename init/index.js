db92b89166381867b23020f40f2a1606a0709aa7
Amit Mehta <samit051027@gmail.com>
2024-06-16 00:32:53 +0530

    Add Project Files

---
+++ init/index.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+main()
+  .then(() => {
+    console.log("connected to database");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+async function main() {
+  await mongoose.connect(MONGO_URL);
+}
+
+const initDB = async () => {
+  await Listing.deleteMany({});
+  initData.data = initData.data.map((obj) => ({
+    ...obj,
+    owner: "666b4167a1ec2698b653ee13",
+  }));
+  await Listing.insertMany(initData.data);
+  console.log("data was intialized");
+};
+
+initDB();
---
+++ utils/ExpressError.js
@@ -0,0 +1,10 @@
+// Custom error hndler for errors
+class ExpressError extends Error {
+  constructor(statusCode, message) {
+    super();
+    this.statusCode = statusCode;
+    this.message = message;
+  }
+}
+
+module.exports = ExpressError;
