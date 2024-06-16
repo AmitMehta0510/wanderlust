4af80fd53fa12e83c7b69e240e60bed6740d1d58
Amit Mehta <samit051027@gmail.com>
2024-06-16 19:51:09 +0530

    Resolved flash message issue

--- routes/listing.js
+++ routes/listing.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
 const Listing = require("../models/listing.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
+
 const ListingController = require("../controllers/listings.js");
 const multer = require("multer");
 const { storage } = require("../cloudConfig.js");
